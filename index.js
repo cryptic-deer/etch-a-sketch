32653fc4b95feffc715d79bd4e432eb7d1145e94
cryptic-deer <artist.tibi@gmail.com>
2022-07-26 17:23:37 +0200

    Add download function

--- index.js
+++ index.js
@@ -111,6 +111,30 @@ function setOption(selected) {
 	}
 }
 
+// save work on click
+function capture() {
+	const captureElement = document.querySelector("#container");
+	html2canvas(captureElement)
+		.then(canvas => {
+			canvas.style.display = "none";
+			document.body.appendChild(canvas);
+			return canvas;
+		})
+		.then(canvas => {
+			const image = canvas
+				.toDataURL("image/png")
+				.replace("image/png", "image/octet-stream");
+			const a = document.createElement("a");
+			a.setAttribute("download", "my-image.png");
+			a.setAttribute("href", image);
+			a.click();
+			canvas.remove();
+		});
+}
+
+const saveButton = document.querySelector(".download");
+saveButton.addEventListener("click", capture);
+
 window.addEventListener("load", () => {
 	generateGrid(size);
 	setOption(option);
